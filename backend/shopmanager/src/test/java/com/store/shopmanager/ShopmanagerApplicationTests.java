10bb1cc2be32dbf7aa302f732c9a792ac1a1cfc8
Saul <saul@ebless.mx>
2023-03-23 11:17:45 -0600

    Springboot added

---
+++ backend/shopmanager/src/test/java/com/store/shopmanager/ShopmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.shopmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
